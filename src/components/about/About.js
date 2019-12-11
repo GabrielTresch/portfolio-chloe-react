427b85349aa26e5e2182354dd98d992f4dcf23fb
GabrielTresch <gabriel_tresch@outlook.com>
2019-12-11 23:33:26 +0100

    about style + responsive

---
+++ src/components/about/About.js
@@ -0,0 +1,28 @@
+/* eslint-disable max-len */
+import React from 'react';
+import ProfilImg from '../../assets/img/chloe-about.jpg';
+import './About.scss';
+
+const About = () => (
+  <div className="about-container">
+    <div className="about-photo">
+      <div className="about-content">
+        <img src={ProfilImg} alt="Chloé Muller" />
+        <div className="about-carre-deco" />
+        <h1>
+          <span>CHLOÉ </span>
+          MULLER
+        </h1>
+      </div>
+    </div>
+    <div className="about-description">
+      <h2>Qui suis-je ?</h2>
+      <p>Je m’appelle Chloé Muller, j’ai 22 ans et je suis en 3ème année de licence en Sciences de l’Education à Mulhouse. </p>
+      <p>
+        A travers ce portfolio réflexif, j’espère vous permettre de mieux cerner ma personnalité, mon caractère, mes buts et mes objectifs dans la vie.
+      </p>
+    </div>
+  </div>
+);
+
+export default About;
